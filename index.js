5773de0f6bdfb03dc3329b3c1e8dc3d782ed7f8c
JMelgar03 <melgarjonathan03@gmail.com>
2021-08-04 22:31:25 +0000

    Pair session finished

--- index.js
+++ index.js
@@ -10,15 +10,51 @@ app.listen(3000, ()=>{
 })
 */
 
-const nombreUsuario = ()=>{
-    return 'Jonathan'
-}
 
-const saludoUsuario = (usuario) =>{
-    return `Hola ${usuario}`;
+const sum = (entrada) =>{
+    if(entrada.length === 0 || entrada=== ''){
+        return 0;
+    }
+
+    const arrNum = entrada.split('');
+    let nums = [];
+
+    
+     //console.log(caracter);
+   
+   
+    if(arrNum[0]==='/' && arrNum[1]==='/'){
+         const caracter = entrada.charAt(2)
+        entrada = entrada.replace(/\n/g, caracter)
+       
+         nums = entrada.split(caracter).map(n => 
+            !isNaN(n) &&  n!==''
+        ? parseFloat(n)
+        : 0)
+
+       // console.log(nums);
+
+    }else{
+         entrada = entrada.replace(/\n/g, ',')
+          nums = entrada.split(',').map(n => parseFloat(n));
+         
+    }
+
+    const negativos = nums.filter(n => n < 0)
+
+    if (negativos.length > 0) {
+        throw new Error(`Negativos no permitidos: ${negativos[0]}`)
+    }
+   
+    let suma = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        suma += nums[i];    
+    }
+
+    return suma
 }
 
 module.exports = {
-    nombreUsuario,
-    saludoUsuario
+    sum
 }
